f927c38eff48fc51a3c33e2dd2aed2c11b3c0eea
Trey Werr <treywerr@gmail.com>
2021-04-10 19:06:39 -0500

    Build out loop

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ canvas.setAttributeNS(null, 'height', '100%');
 canvas.setAttributeNS(null, 'width', '100%');
 document.getElementById('canvas-div').appendChild(canvas);
 
-const L = 200; // pixel length of line segments in baseline.
+const L = 256; // pixel length of line segments in baseline.
 
 let baseline = document.createElementNS('http://www.w3.org/2000/svg', 'path');
 let baselineCoords = [new Point(L, L*2), new Point(L, L), new Point(2*L, L), new Point(2*L, 0)]
@@ -29,25 +29,86 @@ baseline.setAttributeNS(null, 'class', 'baseline');
 
 canvas.appendChild(baseline);
 
-const n = 1; // number of iterations.
+const n = 2; // number of iterations.
+
+let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+let colorIndex = 4;
 
 let prevPoints = baselineCoords;
 let points = [];
 
 for (let i = 1; i <= n; i++) {
+    console.log("n = "+i);
     /* Generate points for line */
-    let l = L/Math.pow(2, n);
-    let p = prevPoints[0];
-    points.push(prevPoints[0]);
-    points.push(new Point(p.x-l, p.y));
-    points.push(new Point(p.x-l, p.y-l));
-    points.push(new Point(p.x, p.y-l));
-    points.push(new Point(p.x, p.y-(2*l)));
 
-    let line = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    let diagline = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    let l = L/Math.pow(2, i);
+    points.push(prevPoints[0]);
+    for (let j = 0; j < prevPoints.length-1; j++) {
+        let p = prevPoints[j];
+        let next = prevPoints[j+1];
+        let v = new Point( next.x - p.x, next.y - p.y ); // Orientation of the parent line segment.
+        console.log("p = ("+p.x+','+p.y+')');
+        
+        if (v.x == 0 && v.y < 0) { // North
+            console.log("North");
+            if (j % 2 == 0) {
+                points.push(new Point(p.x-l, p.y));
+                points.push(new Point(p.x-l, p.y-l));
+                points.push(new Point(p.x, p.y-l));
+                points.push(new Point(p.x, p.y-(2*l)));
+            } else {
+                points.push(new Point(p.x, p.y-l));
+                points.push(new Point(p.x+l, p.y-l));
+                points.push(new Point(p.x+1, p.y-(2*l)));
+                points.push(new Point(p.x, p.y-(2*l)));
+            }
+        }
+        if (v.x > 0 && v.y == 0) { // East
+            console.log("East");
+            if (j % 2 == 0) {
+                points.push(new Point(p.x, p.y-l));
+                points.push(new Point(p.x+l, p.y-l));
+                points.push(new Point(p.x+l, p.y));
+                points.push(new Point(p.x+(2*l), p.y));
+            } else {
+                points.push(new Point(p.x+l, p.y));
+                points.push(new Point(p.x+l, p.y+l));
+                points.push(new Point(p.x+(2*l), p.y+l));
+                points.push(new Point(p.x+(2*l), p.y));
+            }
+        }
+        if (v.x == 0 && v.y > 0) { // South
+            console.log("South");
+            if (j % 2 == 0) {
+                points.push(new Point(p.x+l, p.y));
+                points.push(new Point(p.x+l, p.y+l));
+                points.push(new Point(p.x, p.y+l));
+                points.push(new Point(p.x, p.y+(2*l)));
+            } else {
+                points.push(new Point(p.x, p.y+l));
+                points.push(new Point(p.x-l, p.y+l));
+                points.push(new Point(p.x-1, p.y+(2*l)));
+                points.push(new Point(p.x, p.y+(2*l)));
+            }
+        }
+        if (v.x < 0 && v.y == 0) { // West
+            console.log("West");
+            if (j % 2 == 0) {
+                points.push(new Point(p.x, p.y-l));
+                points.push(new Point(p.x+l, p.y-l));
+                points.push(new Point(p.x+l, p.y));
+                points.push(new Point(p.x+(2*l), p.y));
+            } else {
+                points.push(new Point(p.x-l, p.y));
+                points.push(new Point(p.x-l, p.y-l));
+                points.push(new Point(p.x-(2*l), p.y-l));
+                points.push(new Point(p.x-(2*l), p.y));
+            }
+        }
+    }
 
     /* Creating data string using generated points */
+
     let data = 'M'+points[0].x+' '+points[0].y;
     let diagdata = data;
     for (let i = 1; i < points.length; i++) {
@@ -56,12 +117,19 @@ for (let i = 1; i <= n; i++) {
             diagdata += ' L'+points[i].x+' '+points[i].y;
     }
 
+    /* Create svg paths */
+
+    let line = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    let diagline = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+
     diagline.setAttributeNS(null, 'd', diagdata);
-    diagline.setAttributeNS(null, 'class', 'diagline');
+    diagline.setAttributeNS(null, 'class', 'line');
+    diagline.setAttributeNS(null, 'stroke', colors[++colorIndex % colors.length]);
     canvas.appendChild(diagline);
 
     line.setAttributeNS(null, 'd', data);
     line.setAttributeNS(null, 'class', 'line');
+    line.setAttributeNS(null, 'stroke', colors[++colorIndex % colors.length]);
     canvas.appendChild(line);
 
     prevPoints = points.slice();
